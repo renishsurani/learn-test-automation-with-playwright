8880b84f246916313ee9c6f2219dbe6e3fbabf66
renishsurani <renishsurani9900@gmail.com>
2025-03-12 23:25:47 +0530

    Created function 'AdminLogin'

--- specs/example.spec.js
+++ specs/example.spec.js
@@ -2,23 +2,12 @@
 import { test, expect } from '@playwright/test';
 require("dotenv").config();
 
+const { AuthFunctions } = require("../pages/authFunctions");
+
 test('has title', async ({ page }) => {
-  if (!process.env.BASE_URL) {
-    throw new Error('BASE_URL is not defined in the environment variables');
-  }
-  await page.goto(process.env.BASE_URL);
 
-  // Expect a title "to contain" a substring.
-  // await expect(page).toHaveTitle(/Playwright/);
-  await page.waitForTimeout(5000);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  const AuthFunction = new AuthFunctions(page);
+  
+  await AuthFunction.AdminLogin('Admin', 'admin123');
+  await page.waitForTimeout(1000);
 });
