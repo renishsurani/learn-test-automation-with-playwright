c5726c88d88a531258484b267f49a7f0b7dd91b8
renishsurani <renishsurani9900@gmail.com>
2025-03-13 11:59:18 +0530

    Added test case for 01. Admin login and Employee Search list

--- pages/authFunctions.js
+++ pages/authFunctions.js
@@ -2,12 +2,15 @@ import { expect } from "@playwright/test";
 require("dotenv").config();
 
 export class AuthFunctions{
+
+    // The constructor function
     constructor (page){
         this.page = page;
     }
 
+    // The AdminLogin function to login as an admin user
     async AdminLogin(email, password){
-        await this.page.goto(process.env.BASE_URL);
+        
         await this.page.getByRole('textbox', { name: 'Username' }).fill(email);
         await this.page.getByRole('textbox', { name: 'Password' }).fill(password);
         await this.page.getByRole('button', { name: 'Login' }).click();
