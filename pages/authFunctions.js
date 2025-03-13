d9b38f8e6dc166d2f3edda9637541f4eab2cb383
renishsurani <renishsurani9900@gmail.com>
2025-03-13 16:25:26 +0530

    Modified the login function for both user and admin, added test case for 2. Add new Employee

--- pages/authFunctions.js
+++ pages/authFunctions.js
@@ -8,11 +8,18 @@ export class AuthFunctions{
         this.page = page;
     }
 
-    // The AdminLogin function to login as an admin user
-    async AdminLogin(email, password){
+    // The LogIn function to login as an admin user
+    async LogIn(email, password){
         
         await this.page.getByRole('textbox', { name: 'Username' }).fill(email);
         await this.page.getByRole('textbox', { name: 'Password' }).fill(password);
         await this.page.getByRole('button', { name: 'Login' }).click();
+    }
+
+    // The LogOut function to logout from the application
+    async LogOut() {
+        await this.page.getByRole('banner').getByRole('img', { name: 'profile picture' }).click();
+        await this.page.getByRole('menuitem', { name: 'Logout' }).click();
+        await expect(this.page.getByRole('heading', { name: 'Login' })).toBeVisible();
     }
 } 
---
+++ specs/02-add-new-employee.spec.js
@@ -0,0 +1,48 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+require("dotenv").config();
+
+const { AuthFunctions } = require("../pages/authFunctions");
+const { EmployeeFunctions } = require("../pages/employeeFunctions");
+
+test.describe('Scenarios: 3 Add a New Employee', () => {
+
+  const randomNumber = Math.floor(Math.random() * 999999999).toString();
+  const Username = 'playwright' + randomNumber;
+  const EmployeeInfo = {
+    FirstName: 'Playwright',
+    LastName: 'Playwright',
+    EmployeeID: randomNumber,
+    Username: Username,
+    Password: Username,
+  };
+
+  test('Add new employee', async ({ page }) => {
+    // Create an instance of the AuthFunctions and SearchEmployee classes
+    const AuthFunction = new AuthFunctions(page);
+    const EmployeeFunction = new EmployeeFunctions(page);
+
+    // Navigate to the base URL
+    await page.goto(process.env.BASE_URL);
+
+    // Call the LogIn function from the AuthFunctions class to login as an admin
+    await AuthFunction.LogIn(process.env.WP_USERNAME, process.env.WP_PASSWORD);
+
+    await EmployeeFunction.AddNewEmployee(...Object.values(EmployeeInfo));
+
+  });
+
+  test('Login with new employee credantials', async ({ page }) => {
+    // Create an instance of the AuthFunctions and SearchEmployee classes
+    const AuthFunction = new AuthFunctions(page);
+
+    // Navigate to the base URL
+    await page.goto(process.env.BASE_URL);
+
+    // Call the LogIn function from the AuthFunctions class to login as a new employee user
+    await AuthFunction.LogIn(EmployeeInfo.Username, EmployeeInfo.Password);
+
+    await expect(page.getByText(EmployeeInfo.FirstName + " " + EmployeeInfo.LastName)).toBeVisible();
+
+  });
+});
